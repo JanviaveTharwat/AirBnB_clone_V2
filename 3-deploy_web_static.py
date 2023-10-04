63bd20527445495d0837e33006d7e9e864e2e733
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-10-04 20:55:39 +0200

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,65 +1,14 @@
 #!/usr/bin/python3
 # Fabfile to create and distribute an archive to a web server.
-import os
-from datetime import datetime
-from fabric.api import env, local, put, run, runs_once
-
-env.hosts = ['107.22.142.201', '35.153.57.148']
-
-
-@runs_once
-def do_pack():
-    """Archives the static files."""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
-
-
-def do_deploy(archive_path):
-    """Deploys the static files to the host servers.
-    Args:
-        archive_path (str): The path to the archived static files.
-    """
-    if not os.path.exists(archive_path):
-        return False
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
-    try:
-        put(archive_path, "/tmp/{}".format(file_name))
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        run("rm -rf /tmp/{}".format(file_name))
-        run("mv {}web_static/* {}".format(folder_path, folder_path))
-        run("rm -rf {}web_static".format(folder_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_path))
-        print('New version is now LIVE!')
-        success = True
-    except Exception:
-        success = False
-    return success
+do_pack = __import__('1-pack_web_static')
+do_deploy = __import__('2-do_deploy_web_static')
 
 
 def deploy():
-    """Archives and deploys the static files to the host servers.
     """
-    archive_path = do_pack()
-    return do_deploy(archive_path) if archive_path else False
+    Pack tarball and deploy to servers
+    """
+    filepath = do_pack.do_pack()
+    if not filepath:
+        return False
+    return do_deploy.do_deploy(filepath)
