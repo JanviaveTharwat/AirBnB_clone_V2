3f8de6d700f2be4cee0104e3c1e3e800afea3d1b
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-10-04 07:29:52 +0200

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,16 +12,16 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html>
   <head>
-    </head>
-      <body>
-          Holberton School
-	    </body>
-	    </html>" | sudo tee /data/web_static/releases/test/index.html
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-	    sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-	    sudo chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
-	    sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-	    sudo service nginx restart
+sudo service nginx restart
