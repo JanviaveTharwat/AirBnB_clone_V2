4c3655789369ec72f0991cb285c64df8b47d4360
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-10-08 02:02:31 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,77 +1,57 @@
 #!/usr/bin/python3
-"""a script to pack static content into a tarball
+"""Compress web static package
 """
 from fabric.api import *
-from fabric.operations import put
 from datetime import datetime
-import os
+from os import path
+
 
 env.hosts = ['107.22.142.201', '35.153.57.148']
-
-
-def do_pack():
-    """packages all contents from web_static into .tgz archive
-    """
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
-    local('mkdir -p versions')
-    result = local('tar -cvf versions/web_static_{}.tgz web_static'
-                   .format(now))
-    if result.failed:
-        return None
-    else:
-        return result
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/school'
 
 
 def do_deploy(archive_path):
-    """deploys a static archive to web servers
-    """
-    if not os.path.isfile(archive_path):
-        print('archive file does not exist...')
-        return False
+    """Deploy web files to server
+        """
     try:
-        archive = archive_path.split('/')[1]
-        no_ext_archive = archive.split('.')[0]
-    except:
-        print('failed to get archive name from split...')
-        return False
-    uploaded = put(archive_path, '/tmp/')
-    if uploaded.failed:
-        return False
-    res = run('mkdir -p /data/web_static/releases/{}/'.format(no_ext_archive))
-    if res.failed:
-        print('failed to create archive directory for relase...')
-        return False
-    res = run('tar -C /data/web_static/releases/{} -xzf /tmp/{}'.format(
-               no_ext_archive, archive))
-    if res.failed:
-        print('failed to untar archive...')
-        return False
-    res = run('rm /tmp/{}'.format(archive))
-    if res.failed:
-        print('failed to remove archive...')
-        return False
-    res = run('mv /data/web_static/releases/{}/web_static/* \
-               /data/web_static/releases/{}'
-              .format(no_ext_archive, no_ext_archive))
-    if res.failed:
-        print('failed to move extraction to proper directory...')
-        return False
-    res = run('rm -rf /data/web_static/releases/{}/web_static'
-              .format(no_ext_archive))
-    if res.failed:
-        print('failed to remove first copy of extraction after move...')
-        return False
-    # clean up old release and remove it
-    res = run('rm -rf /data/web_static/current')
-    if res.failed:
-        print('failed to clean up old release...')
-        return False
-    res = run('ln -sfn /data/web_static/releases/{} /data/web_static/current'
-              .format(no_ext_archive))
-    if res.failed:
-        print('failed to create link to new release...')
+        if not (path.exists(archive_path)):
+            return False
+
+        # upload archive
+        put(archive_path, '/tmp/')
+
+        # create target dir
+        timestamp = archive_path[-18:-4]
+        run('sudo mkdir -p /data/web_static/\
+            releases/web_static_{}/'.format(timestamp))
+
+        # uncompress archive and delete .tgz
+        run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
+            /data/web_static/releases/web_static_{}/'
+            .format(timestamp, timestamp))
+
+        # remove archive
+        run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
+
+        # move contents into host web_static
+        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+            /data/web_static/releases/web_static_{}/'
+            .format(timestamp, timestamp))
+
+        # remove extraneous web_static dir
+        run('sudo rm -rf /data/web_static/releases/\
+            web_static_{}/web_static'
+            .format(timestamp))
+
+        # delete pre-existing sym link
+        run('sudo rm -rf /data/web_static/current')
+
+        # re-establish symbolic link
+        run('sudo ln -s /data/web_static/releases/\
+            web_static_{}/ /data/web_static/current'.format(timestamp))
+    except Exception as e:
         return False
 
-    print('\nNew Version Successfuly Deployed!\n')
-
+        # return True on success
     return True
